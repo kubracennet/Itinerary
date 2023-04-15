0e5a278010b54946d33c834f8e227cd9d8f5b6ff
kubracennet <kubracennetyavasoglu@gmail.com>
2023-04-15 22:28:24 +0300

    Initial Commit

---
+++ Itinerary/ItineraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItineraryApp.swift
+//  Itinerary
+//
+//  Created by Kübra Cennet Yavaşoğlu on 15.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ItineraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
