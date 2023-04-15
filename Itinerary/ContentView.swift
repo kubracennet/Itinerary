9b9a5def08d0320c5c3c8f26069d917e7cfe9751
kubracennet <kubracennetyavasoglu@gmail.com>
2023-04-15 23:30:57 +0300

    ItineraryDetails, Curves

--- Itinerary/ContentView.swift
+++ Itinerary/ContentView.swift
@@ -24,7 +24,7 @@ struct ContentView: View {
                     VStack (alignment: .leading, spacing: 8) {
                         HStack {
                             Text("Hi").font(.largeTitle)
-                            Text("Martin").font(.largeTitle).fontWeight(.heavy)
+                            Text("Alex").font(.largeTitle).fontWeight(.heavy)
                         }
                         Text("Let's Discover a New Adventures").font(.subheadline).fontWeight(.light)
                     }
@@ -43,9 +43,115 @@ struct ContentView: View {
                             .cornerRadius(80).shadow(radius: 1)
                             .padding()
                     }
-                }
-            }
+                    
+                    HStack {
+                        VStack {
+                            Button(action: {}) {
+                                Image(systemName: "airplane")
+                                    .foregroundColor(.white)
+                                    .padding(.all, 18)
+                                    .background(Color.blue.opacity(0.8))
+                                    .cornerRadius(12)
+                                    .shadow(radius: 2)
+                            }
+                            Text("Flights").font(.footnote)
+                                .bold()
+                        }
+                        Spacer()
+                        VStack {
+                                Button(action: {}) {
+                                    Image(systemName: "bed.double.fill")
+                                        .foregroundColor(.white)
+                                        .padding(.all, 18)
+                                        .background(Color.orange.opacity(0.8))
+                                        .cornerRadius(12)
+                                        .shadow(radius: 2)
+                                }
+                                Text("Hotels").font(.footnote)
+                                    .bold()
+                            }
+                        Spacer()
+                        VStack {
+                                Button(action: {}) {
+                                    Image(systemName: "location.fill")
+                                        .foregroundColor(.white)
+                                        .padding(.all, 18)
+                                        .background(Color.purple.opacity(0.8))
+                                        .cornerRadius(12)
+                                        .shadow(radius: 2)
+                                }
+                                Text("Places").font(.footnote)
+                                    .bold()
+                            }
+                        Spacer()
+                         VStack {
+                                Button(action: {}) {
+                                    Image(systemName: "rectangle.grid.2x2.fill")
+                                        .foregroundColor(.white)
+                                        .padding(.all, 18)
+                                        .background(Color.red.opacity(0.8))
+                                        .cornerRadius(12)
+                                        .shadow(radius: 2)
+                                }
+                                Text("More").font(.footnote)
+                                    .bold()
+                            }
+                    } .padding()
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Text("Your").font(.largeTitle)
+                        Text("Trips").font(.largeTitle)
+                            .fontWeight(.heavy)
+                    }.padding(.vertical)
+                    
+                    HStack (spacing: 60) {
+                        Text("Futured").bold()
+                        Text("Past").foregroundColor(.secondary)
+                        Text("All").foregroundColor(.secondary)
+
+                    }.padding(.vertical)
+                    
+                    ScrollView (.horizontal, showsIndicators: false) {
+                        HStack (spacing: 20) {
+                            NavigationLink(destination:
+                              ItineraryDetail()) {
+                              Image("london").resizable()
+                                 .aspectRatio(contentMode: .fill).frame(width: 200, height: 270).cornerRadius(12)
+                            }.buttonStyle(PlainButtonStyle())
+                            Image("italy").resizable()
+                                .aspectRatio(contentMode: .fill).frame(width: 200, height: 270).cornerRadius(12)
+                            
+                        }
+                    }.padding(.trailing, -20)
+                }.padding().foregroundColor(Color(UIColor.systemIndigo))
+                    .edgesIgnoringSafeArea(.bottom)
+            }.navigationTitle("")
+                .navigationBarHidden(true)
+        }.accentColor(Color(UIColor.systemIndigo))
+    }
+}
+
+struct ItineraryDetail: View {
+    var body: some View {
+        VStack (alignment: .leading) {
+            VStack {
+                Image("london").resizable().aspectRatio(contentMode: .fill).frame(height: 450)
+            }.clipShape(Curves(corner: .bottomLeft, radius: 35))
+                .clipShape(Curves(corner: .bottomRight, radius: 35))
+                .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
         }
+    }
+}
+
+struct Curves : Shape {
+    var corner: UIRectCorner
+    var radius: CGFloat
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath (roundedRect: rect, byRoundingCorners: corner, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
     }
 }
 
